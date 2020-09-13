c939b7964b82408cebd9d2dd41aada3929b7bca8
Vagios Vlachos <bagios234@gmail.com>
2020-09-13 18:38:46 +0300

    Retire pieces of code from plugin.py

--- resources/lib/plugin.py
+++ resources/lib/plugin.py
@@ -39,10 +39,6 @@ def index():
     addDirectoryItem(plugin.handle, plugin.url_for(
         search), ListItem("Search Anime"), True)
     addDirectoryItem(plugin.handle, plugin.url_for(
-        show_conan), ListItem("Detective Conan"), True)
-    addDirectoryItem(plugin.handle, plugin.url_for(
-        show_grouped_episodes, id = 210, slug = "detective-conan"), ListItem("Detective Conan - Kitsu"), True)
-    addDirectoryItem(plugin.handle, plugin.url_for(
         test_kitsu), ListItem("Test"), True)
     if not settings.have_anilist_token():
         addDirectoryItem(plugin.handle, plugin.url_for(
@@ -121,7 +117,7 @@ def search():
     query = dialog.input("Enter search query")
     logger.debug("Search for: " + str(query))
     search_result = search_anime_kitsu(query)
-    create_anime_list(search_result)
+    # create_anime_list(search_result)
 
 # TODO: Needs refacotring
 @plugin.route('/anime/anilist/<id>/episodes/latest-<latest_episode>/offset-<offset>')
@@ -230,110 +226,6 @@ def get_sources(slug, number):
     # setResolvedUrl(plugin.handle, True, listitem=play_item)
     # print(urls_gogoanime)
     # Player().play(urls_gogoanime[url_index], play_item)
-
-#For Conan
-@plugin.route('/anime/<id>/<slug>/episodes-grouped')
-def show_grouped_episodes(id, slug):
-    # Need to find the exact number of released episodes
-    # Kitsu adds dummy-empty unreleased episodes in long running shows
-    # Until we find a new way to get that from an API we will use gogoanime1
-    latest_episode = get_latest_episode_number(slug)
-    anime = get_anime_by_id(id)
-    # Create a list item per 20 episodes
-    for start in range(1, int(latest_episode), 20):
-        # print("In loop: " + str(start))
-        end = str(int(start) + 19) if (int(start) + 19 <= int(latest_episode)) else str(latest_episode)
-        list_item = ListItem(label=('Episodes ' + str(start) + '-' + str(end)).encode('utf-8'))
-        list_item.setInfo('video', anime.getAnimeInfo())
-        list_item.setArt(anime.getAnimeArt())
-        is_folder = True
-        addDirectoryItem(plugin.handle, plugin.url_for(
-            show_episodes, id = id, slug = slug, offset=str(start), latest_episode=str(latest_episode)), list_item, is_folder)
-    endOfDirectory(plugin.handle)
-    pass
-
-# For Conan and for grouped anilist
-@plugin.route('/anime/kitsu/<id>/<slug>/episodes/latest-<latest_episode>/<offset>')
-def show_episodes(id, slug, latest_episode = -1, offset = 0):
-    # Kitsu will handle episodes so it can show the title
-    episodes = get_anime_episodes(id, offset)
-    for episode in episodes:
-        title = episode.canonicalTitle if episode.canonicalTitle != None else ""
-        if int(episode.number) == int(latest_episode):
-            item_label = ("Episode " + str(episode.number) + "[CR]" + "Not released yet" )
-        else:
-            item_label = ("Episode " + str(episode.number) + "[CR]" + title )
-        list_item = ListItem(label = item_label)
-        list_item.setInfo('video', episode.getEpisodeInfo())
-        list_item.setArt(episode.getEpisodeArt())
-        is_folder = False
-        addDirectoryItem(plugin.handle, plugin.url_for(
-            get_sources, slug, episode.number), list_item, is_folder)
-        if int(episode.number) == int(latest_episode):
-            item_label = ("Episode " + str(episode.number) + "[CR]" + "Not released yet" )
-            break
-    endOfDirectory(plugin.handle)
-
-@plugin.route('/conan')
-def show_conan():
-    for i in range(700, 900, 10):
-        list_item = ListItem(label=("Detective Conan: " + str(i + 1) + " - " + str(i + 10)))
-        # list_item.setLabel2(movie.expiration_date)
-        # list_item.setInfo('video', movie.getMovieInfo())
-        # list_item.setArt(movie.getMovieArt())
-        list_item.setProperty('IsPlayable', 'False')
-        # url = get_mp4_from_url(baseURL)
-        is_folder = True
-        addDirectoryItem(plugin.handle, plugin.url_for(
-            show_conan_episodes, i + 1), list_item, is_folder)
-    endOfDirectory(plugin.handle)
-
-@plugin.route('/conan/<episodes>')
-def show_conan_episodes(episodes):
-    for i in range(int(episodes), int(episodes) + 10):
-        list_item = ListItem(
-            label=("Detective Conan - " + str(i)),
-            offscreen=True)
-        # list_item.setLabel2(movie.expiration_date)
-        # list_item.setInfo('video', movie.getMovieInfo())
-        # list_item.setArt(movie.getMovieArt())
-        list_item.setProperty('IsPlayable', 'False')
-        # url = get_mp4_for_conan(i)
-        is_folder = True
-        list_item
-        addDirectoryItem(
-            plugin.handle,
-            plugin.url_for(play_conan, str(i)),
-            list_item, is_folder)
-    endOfDirectory(plugin.handle)
-
-@plugin.route('/conan/play/<episode>')
-def play_conan(episode):
-    item = ListItem(label=("Play episode"))
-    item.setProperty('IsPlayable', 'True')
-    item.setInfo('video',{
-        'mediatype': "tvshow"
-    })
-    is_folder = False
-    addDirectoryItem(
-            plugin.handle,
-            get_mp4_for_conan(episode),
-            item, is_folder)
-    endOfDirectory(plugin.handle)
-
-def create_anime_list(anime_list):
-    for anime in anime_list:
-        list_item = ListItem(label=anime.canonicalTitle)
-        list_item.setInfo('video', anime.getAnimeInfo())
-        list_item.setArt(anime.getAnimeArt())
-        is_folder = True
-        if (anime.episodeCount != None) and (anime.episodeCount < 80):
-            addDirectoryItem(plugin.handle, plugin.url_for(
-                show_episodes, id = anime.id, slug = anime.slug, latest_episode = -1, offset = 0), list_item, is_folder)
-        else:
-            addDirectoryItem(plugin.handle, plugin.url_for(
-                show_grouped_episodes, id = anime.id, slug = anime.slug), list_item, is_folder)
-    endOfDirectory(plugin.handle)
 
 def run():
     plugin.run()
